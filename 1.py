92b0d12bc3ada308497cf3a299cc3cb7f660e459
TimurTimergalin <timur20041229@gmail.com>
2020-02-27 21:03:15 +0300

    task4

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
-from class1 import Ui_MainWindow
+from class4 import Ui_MainWindow
 import sys
 import requests
 import os
@@ -11,13 +11,20 @@ class YandexMapsAPI(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.ll = [0, 0]
         self.spn = [30.0, 30.0]
+        self.l = ''
         self.static_API()
+        self.mapbox.clicked.connect(self.box_callback)
+        self.satbox.clicked.connect(self.box_callback)
+        self.sklbox.clicked.connect(self.box_callback)
+        self.maptrfsklbox.clicked.connect(self.box_callback)
+        self.satsklbox.clicked.connect(self.box_callback)
+        self.sattrfsklbox.clicked.connect(self.box_callback)
 
     def static_API(self):
         params = {
             'll': str(self.ll[0]) + ',' + str(self.ll[1]),
             'spn': str(self.spn[0]) + ',' + str(self.spn[1]),
-            'l': 'map'
+            'l': self.l if self.l else 'map'
         }
         address = "http://static-maps.yandex.ru/1.x/"
         response = requests.get(address, params=params)
@@ -50,30 +57,35 @@ class YandexMapsAPI(QtWidgets.QMainWindow, Ui_MainWindow):
                 if self.spn[0] < 0.001 or self.spn[1] < 0.001:
                     self.spn = [0.001, 0.001]
                 self.static_API()
-        if e.key() == QtCore.Qt.Key_Up:
+        if e.key() == QtCore.Qt.Key_W:
             if -90 < self.ll[1] < 90:
                 self.ll[1] += 50 * ((self.spn[0] + self.spn[1]) / 2) / 180
                 if not (-90 < self.ll[1] < 90):
                     self.ll[1] = 0
                 self.static_API()
-        if e.key() == QtCore.Qt.Key_Down:
+        if e.key() == QtCore.Qt.Key_S:
             if -90 < self.ll[1] < 90:
                 self.ll[1] -= 50 * ((self.spn[0] + self.spn[1]) / 2) / 180
                 if not (-90 < self.ll[1] < 90):
                     self.ll[1] = 0
                 self.static_API()
-        if e.key() == QtCore.Qt.Key_Right:
+        if e.key() == QtCore.Qt.Key_D:
             self.ll[0] += 50 * ((self.spn[0] + self.spn[1]) / 2) / 180
             if self.ll[0] > 180:
                 self.ll[0] -= 360
             self.static_API()
-        if e.key() == QtCore.Qt.Key_Left:
+        if e.key() == QtCore.Qt.Key_A:
             self.ll[0] -= 50 * ((self.spn[0] + self.spn[1]) / 2) / 180
             if self.ll[0] < -180:
                 self.ll[0] += 360
             self.static_API()
 
         print(e.key())
+
+    def box_callback(self, instance):
+        self.l = self.sender().text()
+        print(self.l, 'new map')
+        self.static_API()
 
 
 def exit_(app):
