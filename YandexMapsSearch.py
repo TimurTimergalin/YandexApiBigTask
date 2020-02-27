db7d441cf28fd77b7705db05f849a7c29a9519a0
TimurTimergalin <timur20041229@gmail.com>
2020-02-27 18:43:32 +0300

    task2

--- YandexMapsSearch.py
+++ YandexMapsSearch.py
@@ -9,8 +9,8 @@ class YandexMapsAPI(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.ll = 0, 0
-        self.spn = 30, 30
+        self.ll = [49, 55]
+        self.spn = [180.0, 90.0]
         self.static_API()
 
     def static_API(self):
@@ -21,13 +21,37 @@ class YandexMapsAPI(QtWidgets.QMainWindow, Ui_MainWindow):
         }
         address = "http://static-maps.yandex.ru/1.x/"
         response = requests.get(address, params=params)
+        try:
+            os.remove('img.png')
+        except FileNotFoundError:
+            pass
         with open('img.png', 'wb') as file:
             file.write(response.content)
         self.image = QtGui.QPixmap('img.png')
         self.show_map()
+        print('changed')
 
     def show_map(self):
         self.label.setPixmap(self.image)
+        print('showed')
+
+    def keyPressEvent(self, e):
+        if e.key() == QtCore.Qt.Key_PageUp:
+            if self.spn[0] < 180 and self.spn[1] < 90:
+                self.spn[0] *= 1.5
+                self.spn[1] *= 1.5
+                if self.spn[0] > 180 or self.spn[1] > 90:
+                    self.spn = [180, 90]
+                print(e.key())
+                self.static_API()
+        if e.key() == QtCore.Qt.Key_PageDown:
+            if self.spn[0] > 0.001 and self.spn[1] > 0.001:
+                self.spn[0] *= 0.5
+                self.spn[1] *= 0.5
+                if self.spn[0] < 0.001 or self.spn[1] < 0.001:
+                    self.spn = [0.001, 0.001]
+                print(e.key())
+                self.static_API()
 
 
 def exit_(app):
